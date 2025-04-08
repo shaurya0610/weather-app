5899ac9eaaf9eb14d84655471a273ea318e9d74c
Shaurya Chaturvedi <shaurya.ch06@gmail.com>
2025-04-08 14:39:53 +0530

    Fetch OpenWeatherMap data for input city

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <SearchBar />
+      <SearchBar /> {/*Lock Search bar to top of screen*/}
       <Weather />
     </>
   )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SearchBar = () => {
+
+    const [query, setQuery] = useState();
+
+    useEffect(() => {
+        if (query) {
+            console.log("Query changed to:", query);
+        }
+    }, [query]);
+
     return (
-        <input
-            type="text"
-            placeholder="Search for a city..."
-            className="px-2 py-2 rounded-xl border"
-        />
+        <>
+            <form action="" onSubmit={(event) => {
+                event.preventDefault();
+                console.log(`Query submitted: ${query}`);
+                async function getWeather() {
+                    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=624537293cd98022c7d7414a3d507efb&units=metric`;
+                    try {
+                        const response = await fetch(url);
+                        if (!response.ok) {
+                            throw new Error(`Response status: ${response.status}`);
+                        }
+
+                        const json = await response.json();
+                        console.log(json);
+                    } catch (error) {
+                        console.error(error.message);
+                    }
+                }
+                getWeather();
+            }}>
+                <input
+                    type="text"
+                    placeholder="Search for a city..."
+                    onChange={(event) => {
+                        setQuery(event.target.value);
+                    }}
+                    className="p-2 rounded-xl border"
+                />
+                <button className='p-2 rounded-xl border opacity-75'>Search</button>
+            </form>
+        </>
     );
 };
 
